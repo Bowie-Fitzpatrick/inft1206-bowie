ddc6bd12f9c0537a68dafbd35cd6327f6481fd57
Bowie <william.fitzpatrick1@dcmail.ca>
2026-04-13 17:36:04 -0400

    added comments

--- assignment4/part2/script.js
+++ assignment4/part2/script.js
@@ -3,3 +3,44 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+// Create a data object
+
+
+// Add the images to the thumbnail bar
+
+
+// Create a constant called baseURL
+
+
+// Create a for of loop
+
+
+// Create a new <img> element for each image
+
+
+// Set the <img> source equal to the URL of the image
+
+
+// Make the images focusable via keyboard
+
+
+// Append <img> to thumbBar
+
+
+// Add a click event to the <img> so that when it is clicked it calls a function called updateDisplayedImage()
+
+
+// Create updateDisplayedImage()
+
+
+// Wire up the Darken/Lighten button
+
+
+// Add a click event to the <button> with an anonymous function
+
+
+// If the <button> has a class of dark when clicked, change its text content to Lighten
+
+
+// If the <button> does not have a class of dark when clicked, change its text content to Darken
